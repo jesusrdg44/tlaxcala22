a442531bb948a94676c21f3b0ba25d436e669a90
jesusrdg44 <jesusguepardo32@gmail.com>
2025-03-18 15:53:43 -0600

    Api production

--- src/config.js
+++ src/config.js
@@ -1,12 +1,21 @@
-const hostApi = process.env.NODE_ENV === "development" ? "http://localhost" : "https://flatlogic-node-backend.herokuapp.com";
-const portApi = process.env.NODE_ENV === "development" ? 8080 : "";
+// Si estás en desarrollo, se usará localhost con el puerto 8080 (o el que uses)
+// En producción, se usará la IP de tu servidor y el puerto 5000.
+const hostApi = process.env.NODE_ENV === "development" 
+  ? "http://localhost" 
+  : "http://67.217.243.37";
+  
+const portApi = process.env.NODE_ENV === "development" 
+  ? 8080 
+  : 5000;
+
 const baseURLApi = `${hostApi}${portApi ? `:${portApi}` : ``}`;
 
 export default {
   hostApi,
   portApi,
   baseURLApi,
-  remote: "https://flatlogic-node-backend.herokuapp.com",
+  // Si no usas otras variables, puedes actualizar también 'remote' para que apunte a tu backend
+  remote: baseURLApi,
   isBackend: process.env.REACT_APP_BACKEND,
   auth: {
     email: 'admin@flatlogic.com',
